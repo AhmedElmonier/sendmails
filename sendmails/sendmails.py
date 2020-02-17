78e78fa8be1172d39c6cac209c9034e9321696c0
AhmedElmonier <54212985+AhmedElmonier@users.noreply.github.com>
2020-02-17 08:12:59 +0300

    Update

--- sendmails/sendmails.py
+++ sendmails/sendmails.py
@@ -2,15 +2,40 @@ import os
 import smtplib
 from email.message import EmailMessage
 
-email_address = os.environ.get('SM_USER')
-password = os.environ.get('SM_PASS')
+user = os.environ.get('IMPEX_EMAIL')
+password = os.environ.get('IMPEX_PASS')
+
 
 msg = EmailMessage()
 msg['Subject'] = 'Hello There'
-msg['From'] = email_address
-msg['To'] = 'ahmedonea2016@gmail.com'
-msg.set_content('Hello Beauty')
+msg['From'] = user
+msg['To'] = 'elmonierahmed@outlook.com'
+msg.set_content('Hello There')
 
 with smtplib.SMTP('smtp.office365.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo
+    smtp.login(user, password)
     smtp.send_message(msg)
+    print('done!')
+    smtp.close()
 
+
+
+
+
+
+
+#sendto = 'elmonierahmed@outook.com'
+#smtpsrv = "smtp.office365.com"
+#smtpserver = smtplib.SMTP(smtpsrv,587)
+
+#smtpserver.ehlo()
+#smtpserver.starttls()
+#smtpserver.ehlo
+#smtpserver.login(user, password)
+#msgbody = 'This is a test Email send using Python'
+#smtpserver.sendmail(user, sendto, msgbody)
+#print('done!')
+#smtpserver.close()

629003e3005141049364981f37bf7bf9a9977a45
AhmedElmonier <54212985+AhmedElmonier@users.noreply.github.com>
2020-02-17 19:13:29 +0300

    Update

--- sendmails/sendmails.py
+++ sendmails/sendmails.py
@@ -1,6 +1,15 @@
 import os
+import csv
 import smtplib
 from email.message import EmailMessage
+import pandas as pd
+
+
+
+email = pd.read_csv('emails1.csv', delimiter=',')
+
+print(email)
+print(type(email))
 
 user = os.environ.get('IMPEX_EMAIL')
 password = os.environ.get('IMPEX_PASS')
@@ -9,33 +18,13 @@ password = os.environ.get('IMPEX_PASS')
 msg = EmailMessage()
 msg['Subject'] = 'Hello There'
 msg['From'] = user
-msg['To'] = 'elmonierahmed@outlook.com'
+msg['To'] = email
 msg.set_content('Hello There')
 
+
 with smtplib.SMTP('smtp.office365.com', 587) as smtp:
-    smtp.ehlo()
     smtp.starttls()
-    smtp.ehlo
     smtp.login(user, password)
     smtp.send_message(msg)
     print('done!')
     smtp.close()
-
-
-
-
-
-
-
-#sendto = 'elmonierahmed@outook.com'
-#smtpsrv = "smtp.office365.com"
-#smtpserver = smtplib.SMTP(smtpsrv,587)
-
-#smtpserver.ehlo()
-#smtpserver.starttls()
-#smtpserver.ehlo
-#smtpserver.login(user, password)
-#msgbody = 'This is a test Email send using Python'
-#smtpserver.sendmail(user, sendto, msgbody)
-#print('done!')
-#smtpserver.close()
